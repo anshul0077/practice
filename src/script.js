be6e354447757c443d29b0b8cbe0b908f441fbb4
Anshul <choudharyanshul220@gmail.com>
2025-02-07 22:30:56 +0530

    color generator

--- src/script.js
+++ src/script.js
@@ -1,28 +1,87 @@
-let arr=[0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"]
-let stati=""
-let gradient=""
+let arr=[1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"]
+let main=document.querySelector(".main")
+let btnGC=document.querySelector("#btnGC")
+let color1=document.querySelector("#c1")
+let color2=document.querySelector("#c2")
+let cbtn=document.querySelector("#btnC")
+let div=document.querySelector(".div")
+let btnGG=document.querySelector("#btnGG")
+ btnGG.addEventListener("click",()=>{
+   let OGG= one()
+    let TGG=two()
 
-function generatorone(){
-    let hexcode="#"
-    for(let i=0;i<=6;i++){
-        let random=Math.floor((Math.random()*16))
-      let reslut=arr[random]
-     hexcode+=reslut
+console.log(OGG);
+console.log(TGG);
+let graidentColor= `linear-gradient(to right ,${OGG},${TGG})`
+main.style.background=graidentColor
+color1.innerHTML=OGG
+color2.innerHTML=TGG
+
+
+    function one(){
+        let hexcode="#"
+        for(let i=0;i<6;i++){
+            let random=Math.floor(Math.random()*15)
+            let result=arr[random]
+        
+            hexcode+=result    
+        }
+        // console.log(hexcode);
+        return hexcode
+        
+    }
+    function two(){
+
+        let hexcode2="#"
+        for(let i=0;i<6;i++){
+            let random=Math.floor(Math.random()*15)
+            let result=arr[random]
+        
+            hexcode2+=result    
+        }
+        // console.log(hexcode2);
+        return hexcode2
+        
     
     }
-    console.log(hexcode);
-}
-function generatortwo(){
-    let hexcode="#"
 
-    for(let i=0;i<=6;i++){
-        let random=Math.floor((Math.random()*16))
-      let reslut=arr[random]
-     hexcode+=reslut
-    
+})
+btnGC.addEventListener("click",()=>{
+ let oo=   one()
+ main.style.background=""
+ color2.innerHTML=""
+    main.style.backgroundColor=oo
+color1.innerHTML=oo
+
+
+
+    function one(){
+
+        let hexcode="#"
+        for(let i=0;i<6;i++){
+            let random=Math.floor(Math.random()*15)
+            let result=arr[random]
+            hexcode+=result    
+        }
+        return hexcode
     }
-    console.log(hexcode);
-}
-generatorone()
-generatortwo()
+    
+    })
+    cbtn.addEventListener("click",()=>{
+        let color = document.getElementById("c1").innerHTML;  
+        let color22=document.getElementById("c2").innerHTML;
+        
+             navigator.clipboard.writeText(color);  
+             navigator.clipboard.writeText(color22);  
+            // console.log('Content copied to clipboard 2');  
+               
+            // console.log('Content copied to clipboard 1');  
+            // setTimeout(()=>{
+            //     div.classList.remove("hidden")
 
+            // },100)
+            // setTimeout(()=>{
+            //     div.classList.add("hidden")
+
+            // },2000)
+    })
