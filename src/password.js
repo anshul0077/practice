daee459250f19ba8b7c5796af15a237512e241e0
Anshul <choudharyanshul@gmail.com>
2025-02-18 22:43:37 +0530

    final stage

--- src/password.js
+++ src/password.js
@@ -1,50 +1,70 @@
-let lengthInput=document.querySelector("#length").value
-console.log("range inout",lengthInput);
-
-let options=document.querySelector(".options")
-options.addEventListener("click",function(e){
- if(e.target.tagName==="INPUT"){
-    console.log(e.id);
-    
- }
-})
 
 
-
-// logic part
 let clutter=""  
 let passwordLength=10
+let display=document.querySelector("#display")
 let upcase=[ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
 let lwcase=[ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 let num=[1,2,3,4,5,6,7,8,9,0]
 let special=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", "\"", "<", ">", ",", ".", "?", "/"]
- let rf=refersh()
-function refersh(){
-    let uservalue=""
-    uservalue=upcase
-    return uservalue
-}
+let randomvariable=[]
 
-
-
-
-
-let randomvariable=[num]
-randomvariable.push(rf)
-for(let i=0;i<=10;i++){
-    let rv=Math.floor(Math.random()*randomvariable.length)
-    let mainfxn=randomvariable[rv]
-    
-    pass(mainfxn)
-}
-
-function pass (arr){
-    console.log("length", arr.length)
-
-
-    let randomNumber
-        randomNumber=(Math.floor(Math.random()*arr.length)) 
-        clutter+= arr[randomNumber]
-  
+//selector
+let lengthinput=document.querySelector("#lengthinput")
+let lower=document.querySelector("#lowercaseCb")
+let upper=document.querySelector("#uppercaseCb")
+let digit=document.querySelector("#digitsCb")
+let speci=document.querySelector("#specialsCb")
+let btn =document.querySelector(".generate")
+function selector(){
+    if(lower.checked){
+        console.log("lower is checkd"); 
+        randomvariable.push(lwcase)
+        
     }
+    if(upper.checked){
+        console.log("upper is checkd");
+        randomvariable.push(upcase)
+        
+    }
+    if(digit.checked){
+        console.log("digi is checkd");
+        randomvariable.push(num)
+
+        
+    }
+    if(speci.checked){
+        console.log("speci is checkd");
+        randomvariable.push(special)
+        
+    }
+    
+}
+btn.addEventListener('click',function(){
+randomvariablegenerator()
+})
+function randomvariablegenerator(){
+let passlength=Number( lengthinput.value)
+console.log("thsi",passlength);
+
+ clutter=''
+
+ for(let i=0;i<passlength;i++){
+        let rv=Math.floor(Math.random()*randomvariable.length)
+        let  mainfxn=randomvariable[rv]
+console.log(mainfxn.length)
+
+let randomNumber2
+    randomNumber2=(Math.floor(Math.random()*mainfxn.length)) 
+    clutter+= mainfxn[randomNumber2]
+    
     console.log(clutter);
+
+    display.innerHTML=clutter
+  
+}
+}
+
+
+    
+    
